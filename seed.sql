e4c91f4dcefb9c4a1cd5ed5545f9a537c3f5b200
Lauren Powers <laurenpowers@Laurens-MacBook-Air.local>
2022-11-17 15:30:45 -0500

    complete tables and successfully seed

--- seed.sql
+++ seed.sql
@@ -6,8 +6,13 @@ ALTER SEQUENCE information_id_seq RESTART WITH 1;
 
 
 INSERT INTO contacts(contact_name, date_of_birth) VALUES ('Lemmy Kilmister', 1945); 
+INSERT INTO contacts(contact_name, date_of_birth) VALUES ('Kim Deal', 1961);
+INSERT INTO contacts(contact_name, date_of_birth) VALUES ('Tom Waits', 1949);
 INSERT INTO contacts(contact_name, date_of_birth) VALUES ('Kathleen Hanna', 1968); 
 
 INSERT INTO information(email, city, contacts_id) VALUES ('aceofspades@motorhead.com','London, England', 1);
-INSERT INTO information(email, city, contacts_id) VALUES ('rebelgirl@bikinkill.com','New York, NY', 2);
+INSERT INTO information(email, city, contacts_id) VALUES ('rebelgirl@bikinikill.com','New York, NY', 2);
+INSERT INTO information(email, city, contacts_id) VALUES ('tom@jockeyfullofbourbon.com', 'New York, NY', 3);
+INSERT INTO information(email, city, contacts_id) VALUES ('kim@cannonball.com', 'Dayton, OH', 4);
+
 
